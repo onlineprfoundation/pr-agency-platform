13abae712e444b9457e0e174904490ff130aafd9
quickprapp <apple@Apples-MacBook-Pro.local>
2026-03-17 10:51:47 +0530

    Fix client registration and header logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,8 +17,12 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <a href="{{ route('home') }}" class="flex items-center justify-center">
+                    @if(\App\Models\Setting::get('logo_path'))
+                        <img src="{{ \Illuminate\Support\Facades\Storage::url(\App\Models\Setting::get('logo_path')) }}" alt="{{ \App\Models\Setting::get('site_name', config('app.name')) }}" class="h-16 object-contain">
+                    @else
+                        <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    @endif
                 </a>
             </div>
 
